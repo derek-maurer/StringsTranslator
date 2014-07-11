98ba197af2a602fda3056ed629a3e97d132f287d
Derek Maurer <derek@mocaapps.com>
2014-07-11 00:49:19 -0700

    Initial Commit

---
+++ StringsTranslator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  StringsTranslator
+//
+//  Created by Derek Maurer on 7/11/14.
+//  Copyright (c) 2014 Moca Apps. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
